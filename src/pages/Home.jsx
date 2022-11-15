8f8007d8f16f36af450dca4104639d47ce573b3e
Edema-wxd <Edema-wxd@users.noreply.github.com>
2022-11-15 21:42:26 +0100

    refactoring

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,100 @@
-import React from "react";
+import Card from "./shared/Card";
+import mtnftimg from "../images/mtnftimg.svg";
+import mb from "../images/mbtoken.svg";
+import meta from "../images/metamask.svg";
+import op from "../images/opensea.svg";
+import timg from "../images/timg.png";
+import img1 from "../images/Frame1.png";
+import img2 from "../images/Frame 151-1.png";
+import img3 from "../images/Frame1.png";
+import img4 from "../images/Frame1.png";
+import img5 from "../images/Frame1.png";
+import img6 from "../images/Frame1.png";
+import img7 from "../images/Frame1.png";
+import img8 from "../images/Frame1.png";
 
 function Home() {
-	return <div>Home</div>;
+	return (
+		<>
+			<div className="tt">
+				<div className="ttxt">
+					<h1>
+						Rent a <span className="purp">Place</span> away from
+						<span className="purp">Home</span> in the{" "}
+						<span className="purp">Metaverse</span>
+					</h1>
+					<p className="ttpt">
+						we provide you access to luxury and affordable houses in the
+						metaverse, get a chance to turn your imagination to reality at your
+						comfort zone
+					</p>
+					<input
+						type="search"
+						name=""
+						id=""
+						placeholder="Search for location"
+					/>
+				</div>
+				<div className="ttimg">
+					<img src={timg} alt="" />
+				</div>
+			</div>
+			<div className="bb">
+				<div>
+					<img src={mb} alt="" />
+				</div>
+				<div>
+					<img src={meta} alt="" />
+				</div>
+				<div>
+					<img src={op} alt="" />
+				</div>
+			</div>
+			<h4 className="hca">Inspiration for your next adventure</h4>
+			<div className="cardarray">
+				<Card>
+					<img src={img1} alt="" />
+				</Card>
+				<Card>
+					<img src={img2} alt="" />
+				</Card>
+				<Card>
+					<img src={img3} alt="" />
+				</Card>
+				<Card>
+					<img src={img4} alt="" />
+				</Card>
+				<Card>
+					<img src={img5} alt="" />
+				</Card>
+				<Card>
+					<img src={img6} alt="" />
+				</Card>
+				<Card>
+					<img src={img7} alt="" />
+				</Card>
+				<Card>
+					<img src={img8} alt="" />
+				</Card>
+			</div>
+
+			<div className="mtnft">
+				<div className="mtnfttxt">
+					<h4>Metabnb NFTs</h4>
+					<p className="mtp">
+						Discover our NFT gift cards collection. Loyal customers gets amazing
+						gift cards which are traded as NFTs. These NFTs gives our customers
+						access to loads of our exclusive services.
+					</p>
+
+					<button className="mtbtn">Learn more</button>
+				</div>
+				<div className="mtnftimg">
+					<img src={mtnftimg} alt="" />
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Home;
